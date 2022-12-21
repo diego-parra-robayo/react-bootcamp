ff1dda53b0d1995991c6ba6767f73c54a058688e
Diego Parra <diego.parra@wizeline.com>
2022-12-21 14:38:37 -0500

    refactor: snackbar

--- src/ui/base-components/Snackbar.js
+++ src/ui/base-components/Snackbar.js
@@ -3,24 +3,31 @@ import { Component } from "react";
 
 const SnackbarContainer = styled.div`
   min-width: 250px;
-  margin-left: -125px;
-  background-color: #333;
+  background-color: rgba(44, 44, 44, 0.8);
   color: #fff;
-  text-align: center;
-  border-radius: 2px;
-  padding: 16px;
+  border-radius: 4px;
+  padding: 1rem;
+  font-size: 1rem;
+  text-align: start;
   position: fixed;
   z-index: 1;
-  left: 50%;
-  bottom: 30px;
-  font-size: 1rem;
+  left: 3vw;
+  bottom: 3vh;
 `;
 
 class Snackbar extends Component {
-  state = {
-    message: "",
-    visible: false,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      visible: false,
+    };
+  }
+
+  render() {
+    if (!this.state.visible) return null;
+    return <SnackbarContainer>{this.state.message}</SnackbarContainer>;
+  }
 
   show(message, timeout = 2000) {
     this.setState({ visible: true, message }, () => {
@@ -28,10 +35,6 @@ class Snackbar extends Component {
         this.setState({ visible: false });
       }, timeout);
     });
-  }
-  render() {
-    if (!this.state.visible) return null;
-    return <SnackbarContainer>{this.state.message}</SnackbarContainer>;
   }
 }
 
